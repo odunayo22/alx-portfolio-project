e1fb64c714a41f5adc3ba1c0f33eff49d14ef3e4
odunayo22 <shakhordun@gmail.com>
2023-11-01 14:23:58 +0100

    rendering

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,11 @@
-
+import React from 'react';
+import Nav from './components/Nav/Nav';
 //import './App.css';
 
-function App() {
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+     <Nav />
     </div>
   );
 }
